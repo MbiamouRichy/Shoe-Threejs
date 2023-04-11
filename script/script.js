d96f1e5bafc7d29a2d823b336e87058650893c7f
Richy <richymbiamou52@gmail.com>
2023-04-11 20:53:32 +0100

    Mise en place final de mon objet

--- script/script.js
+++ script/script.js
@@ -1,129 +1,83 @@
-import * as THREE from 'three';
-import { OrbitControls } from '../three/examples/jsm/controls/OrbitControls.js';
-import { FBXLoader } from '../three/examples/jsm/loaders/FBXLoader.js'
-
+import * as THREE from "three";
+import { FBXLoader } from "../three/examples/jsm/loaders/FBXLoader.js";
 
 let container;
 
 let camera, scene, renderer;
 
-let mouseX = 0, mouseY = 0;
+let mouseX = 0,
+  mouseY = 0;
 
 let windowHalfX = window.innerWidth / 2;
 let windowHalfY = window.innerHeight / 2;
 
 let object;
-let objectObj
 
 init();
 animate();
 
-
 function init() {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+  camera = new THREE.PerspectiveCamera(25, window.innerWidth / window.innerHeight, 0.1, 2000);
+  camera.position.set(0,0,60)
 
-    container = document.createElement('div');
-    document.body.appendChild(container);
+  // scene
+  scene = new THREE.Scene();
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+  scene.add(ambientLight);
 
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.set(-200, 200, -500);
-   
-    // scene
+  const pointLight = new THREE.PointLight(0xcccccc, 0.8);
+  camera.add(pointLight);
+  scene.add(camera);
 
-    scene = new THREE.Scene();
-    const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-    scene.add(ambientLight);
+  renderer = new THREE.WebGLRenderer({alpha: true});
+  renderer.setPixelRatio(window.devicePixelRatio * 2);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-    const pointLight = new THREE.PointLight(0xcccccc, 0.8);
-    camera.add(pointLight);
-    scene.add(camera);
+  container.appendChild(renderer.domElement);
 
-    // manager
-
-    function loadModel() {
-
-        object.traverse(function (child) {
-
-            if (child.isMesh) child.material.map = texture;
-
-        });
-
-        object.position.y = 0;
-        object.position.x = 0;
-        scene.add(object);
-
-    }
-
-    function onProgress(xhr) {
-
-        if (xhr.lengthComputable) {
-
-            const percentComplete = xhr.loaded / xhr.total * 100;
-            console.log('model ' + Math.round(percentComplete, 2) + '% downloaded');
-        }
-
-    }
-
-    function onError() { }
-    const manager = new THREE.LoadingManager(loadModel);
-    // texture Rock
-    const textureLoader = new THREE.TextureLoader(manager);
-    const texture = textureLoader.load('./assets/Model/Shoe_Color.jpg');
-
-    // const loader = new FBXLoader(manager);
-    // loader.load('./assets/shoe/source/Shoe/Model/Shoe.FBX', function (obj) {
-    //     object = obj;
-    //     object.scale.set(5,5,5)
-    // }, onProgress, onError);
-
-    const loadObj = new FBXLoader();
-    loadObj.load('./assets/sci-fi-shoe_2/source/Shoe.fbx', function (obj) {
-        objectObj = obj;
-        objectObj.scale.set(10,10,10)
-        scene.add(objectObj)
-    }, onProgress, onError);
-
-    renderer
-
-    renderer = new THREE.WebGLRenderer({alpha: true});
-    renderer.setPixelRatio(window.devicePixelRatio * 2);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    container.appendChild(renderer.domElement);
-
-    document.addEventListener('mousemove', onDocumentMouseMove);
-    window.addEventListener('resize', onWindowResize);
-
+  document.addEventListener("mousemove", onDocumentMouseMove);
+  window.addEventListener("resize", onWindowResize);
 }
 
+function onProgress(xhr) {
+  if (xhr.lengthComputable) {
+    const percentComplete = (xhr.loaded / xhr.total) * 100;
+    console.log("model " + Math.round(percentComplete, 2) + "% downloaded");
+  }
+}
+
+function onError() {}
+
+const loader = new FBXLoader();
+loader.load('../assets/shoeNike.fbx',function (obj) {
+    object = obj;
+    object.scale.set(2.6, 2.6, 2.6);
+    object.position.set(0,-7,0)
+    object.rotation.y = 1.6;
+    scene.add(object);
+
+  },onProgress,onError);
+
 function onWindowResize() {
+  windowHalfX = window.innerWidth / 2;
+  windowHalfY = window.innerHeight / 2;
 
-    windowHalfX = window.innerWidth / 2;
-    windowHalfY = window.innerHeight / 2;
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function onDocumentMouseMove(event) {
-    mouseX = (event.clientX - windowHalfX);
-    mouseY = (event.clientY - windowHalfY);
+  mouseX = event.clientX - windowHalfX;
+  mouseY = event.clientY - windowHalfY;
 }
-
-// control
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.target.set( 0, 0.5, 0 );
-controls.update();
-controls.enablePan = true;
-controls.enableDamping = true;
-
-
 function animate() {
-    requestAnimationFrame(animate);
-    render();
+  requestAnimationFrame(animate);
+  render();
 }
 function render() {
-  renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
